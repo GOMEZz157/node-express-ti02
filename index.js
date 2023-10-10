b558fde2e39a4acec0edc1d3ec71358a803a6e5c
Nicollas Gomes <nicksss.gomes@gmail.com>
2023-10-10 14:21:33 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express")
 const path = require("path")
+
 const app = express()
+
 const caminhoBase = path.join(__dirname, "templates")
+
+//https://localhost:3000/usuarios/20
+app.get('/usuarios/:id', (requisicao,resposta) =>{
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados de usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+//https://localhost:3000
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
     
